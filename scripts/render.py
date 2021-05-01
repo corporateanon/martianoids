d6ce1866cf045e0e29510a7c84c6631503302b5f
no <no>
2021-05-02 00:01:18 +0300

    chore: cleanup

--- scripts/render.py
+++ scripts/render.py
@@ -16,41 +16,6 @@ def disableRenderForAllCharacters():
         if col.name.startswith("Character."):
             disableRender(col)
 
-# region 
-def rotateAndRender(characterName):
-    cameraRadius = 1.5
-    lightRadius = 10
-    angleLightDelta = 45 * math.pi/180
-    angularStep = 30
-
-    bpy.context.scene.render.image_settings.file_format = 'PNG'
-
-    for angle in range(0, 359, angularStep):
-        print('--------------------------------')
-        print('Rendering from angle: {}'.format(angle))
-        print('--------------------------------')
-        print('')
-
-        angleRad = angle * math.pi/180
-
-        bpy.data.objects["MainCamera"].location = (
-            math.cos(angleRad) * cameraRadius,
-            math.sin(angleRad) * cameraRadius,
-            cameraRadius
-        )
-
-        bpy.data.objects["MainLight"].location = (
-            math.cos(angleRad+angleLightDelta) * lightRadius,
-            math.sin(angleRad+angleLightDelta) * lightRadius,
-            lightRadius
-        )
-
-        bpy.context.scene.render.filepath = "//renders/{characterName}/render_{angle:03d}".format(angle=angle, characterName=characterName)
-
-        res = bpy.ops.render.render(write_still=True)
-
-# endregion
-
 def degToRad(deg):
     return deg * math.pi/180
 
