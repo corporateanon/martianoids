db3f1765cd554381ec43683fcaa3e66898e7f9ac
no <no>
2021-05-01 23:58:46 +0300

    change(gfx:sprites): adapt JSON for PIXI

--- scripts/yarn-gfx-sprites.js
+++ scripts/yarn-gfx-sprites.js
@@ -8,17 +8,39 @@ const { dirname, join, basename } = require('path');
 
 const OUTPUT_FILE = '../spritesheets/characters.png';
 
-const processCoordinates = (coords) =>
-    Object.fromEntries(
+const processCoordinates = (coords, image) => {
+    const frames = Object.fromEntries(
         Object.entries(coords).map(([key, value]) => {
             const keyParts = key.split('/');
             const [state, characterName] = [
                 keyParts.pop().replace(/\.png$/, ''),
                 keyParts.pop().replace(/^Character\./, ''),
             ];
-            return [`${characterName}.${state}`, value];
+
+            const pixiSpritesheetEntry = {
+                frame: {
+                    x: value.x,
+                    y: value.y,
+                    w: value.width,
+                    h: value.height,
+                },
+                sourceSize: {
+                    w: value.width,
+                    h: value.height,
+                },
+            };
+
+            return [`${characterName}.${state}`, pixiSpritesheetEntry];
         })
     );
+
+    return {
+        meta: {
+            image,
+        },
+        frames,
+    };
+};
 
 const main = async () => {
     const sprites = await globby('../renders/*/*.png');
@@ -34,7 +56,10 @@ const main = async () => {
     const outputStream = fs.createWriteStream(OUTPUT_FILE);
     result.image.pipe(outputStream);
 
-    const processedCoords = processCoordinates(result.coordinates);
+    const processedCoords = processCoordinates(
+        result.coordinates,
+        basename(OUTPUT_FILE)
+    );
     await fs.promises.writeFile(
         outputJson,
         JSON.stringify(processedCoords, false, 2)
